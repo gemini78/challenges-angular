9e17154f8b05c9e4ae404aca14613e6fbef7a16b
Stebou <stebou8mendy@gmail.com>
2021-12-13 18:13:01 +0100

    Added Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoxComponent } from './components/box/box.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, BoxComponent, UserProfileComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    BoxComponent,
+    UserProfileComponent,
+    SignUpComponent,
+    MenuComponent,
+  ],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+const ROUTES: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'user', component: UserProfileComponent },
+];
+
+export { ROUTES };
