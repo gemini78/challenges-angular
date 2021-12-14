89771e8ac8cdce32d4f4bf9e6ee4f95c661070bf
Stebou <stebou8mendy@gmail.com>
2021-12-14 20:52:17 +0100

    Added challenge5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListDonkeyComponent } from './components/list-donkey/list-donkey.component';
 import { UserDonkeyComponent } from './components/user-donkey/user-donkey.component';
 import { RacineComponent } from './components/racine/racine.component';
 import { CreateDonkeyComponent } from './components/create-donkey/create-donkey.component';
 import { ROUTES } from './app.routes';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { ROUTES } from './app.routes';
     RacineComponent,
     CreateDonkeyComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(ROUTES)],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/racine/racine.component.ts
+++ src/app/components/racine/racine.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Donkey } from 'src/app/models/donkey.model';
+import { DonkeyService } from 'src/app/services/donkey.service';
 
 @Component({
   selector: 'app-racine',
   templateUrl: './racine.component.html',
-  styleUrls: ['./racine.component.scss']
+  styleUrls: ['./racine.component.scss'],
 })
 export class RacineComponent implements OnInit {
+  donkeysAvailable: Donkey[];
+  donkeySelected: Donkey | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private donkeyService: DonkeyService) {
+    this.donkeysAvailable = this.donkeyService.donkeysAvailable;
   }
 
+  ngOnInit(): void {}
+
+  selectedDonkey(donkey: Donkey) {
+    //console.log(donkey);
+    this.donkeySelected = donkey;
+    this.removeDonkeyofAvailableArray(donkey);
+  }
+
+  removeDonkeyofAvailableArray(donkey: Donkey) {
+    this.donkeyService.removeDonkeyofAvailableArray(donkey);
+  }
 }
