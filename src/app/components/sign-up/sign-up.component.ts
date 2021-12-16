b71eef1e65b1926c19df3e915168574962d3d2c6
Stebou <stebou8mendy@gmail.com>
2021-12-16 18:05:45 +0100

    Added sign-up component

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  user: User = new User();
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(addUserForm: NgForm) {
+    console.log('onSubmit:', addUserForm);
+    //console.log('onSubmit:', addUserForm.controls['firstname'].errors);
   }
-
 }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+  id: number | undefined;
+  firstname: string | undefined;
+  lastname: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+}
